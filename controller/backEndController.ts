b8f4563185a0cc122c221f63c88d848b81fbeca2
schuler-henry <72646334+schuler-henry@users.noreply.github.com>
2022-04-30 18:47:42 +0200

    Change for debug purpose

--- controller/backEndController.ts
+++ controller/backEndController.ts
@@ -232,7 +232,7 @@ export class BackEndController {
     try {
       content = fs.readFileSync(`${process.cwd()}${filePath}`, 'utf-8');
     } catch (error) {
-      content = "# This file does not exist!";
+      content = "# This file does not exist!" + process.cwd();
     }
 
     return content;
